6b8df67c60852c396907288f03bb12479b0fb963
Autzen <kristyskisfast@gmail.com>
2020-08-30 14:57:59 -0700

    sunday changes

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # sqlalchemy-challenge
+
+Kristy Anderson
+August 30, 2020
+Data Analytics
+SQLAlchemy-Challenge Homework
+
+
+We were tasked to use SQLAlchemy and Flask API to explore and analyze climate data from a hawaii.sqlite database.
+Analyis was performed using Python and Pandas (Jupyter Notebook).
+
+Step 1:
+Climate Analysis and Expoloraition
+Preciption Analyis
+Station Analysis
+
+Step 2:
+Climate App
--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 import sqlalchemy
-from sqlalchemy.ext.automap import automap_base #to pull in the tables in the sqllite db
+from sqlalchemy.ext.automap import automap_base #to pull in the tables from the sqllite db
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
@@ -10,7 +10,7 @@ from flask import Flask, jsonify #for pulling out endpoints and display in brows
 #################################################
 # Database Setup
 #################################################
-engine = create_engine("sqlite:///../Resources/hawaii.sqlite")
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
 Base = automap_base()
@@ -21,73 +21,113 @@ Base.prepare(engine, reflect=True)
 Measurements = Base.classes.measurement #assuming know there is this table
 Stations = Base.classes.station
 
-# Initialize your Flask app
+# Initialize Flask app
 app = Flask(__name__)
 
 # Flask Routes
-
 @app.route("/")
 def home():
     """List all available api routes."""
     return (
         f"Available Routes:<br/>"
-        f"<a href='/api/v1.0/measurement'>measurement</a><br/>"
-        f"<a href='/api/v1.0/station'>station</a>"
-    )
+        f"<a href='/api/v1.0/station'>Stations</a><br/>"
+        f"<a href='/api/v1.0/precipitation'>Precipitation</a><br/>"
+        f"<a href='/api/v1.0/tobs'>TOBS</a><br/>"
+        f"<a href='/api/v1.0/<start>'>Start Date</a><br/>"
+        f"<a href='/api/v1.0/<start>/<end>'>Start Date / End Date</a><br/>"
+        )
 
-@app.route("/api/v1.0/stations")
+#################################################
+# Return a JSON list of stations from the dataset.
+@app.route("/api/v1.0/station")
 def stations():
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
     """Return a list of all station names"""
     # Query all stations
-    results = session.query(stations.station).all() #to get the station name column
+    results = session.query(Stations.station).all() #to get the station name column
 
-    session.close() # good housekeeping to close session
-    print(results)
+    session.close() # good housekeeping to close session (just at end?)
     # Convert list of tuples into normal list
     all_stations = list(np.ravel(results))
 
-# * Return a JSON list of stations from the dataset.
     return jsonify(all_stations)
+
+#################################################
+# * Convert the query results to a dictionary using `date` as the key and `prcp` as the value.
+# * Return the JSON representation of your dictionary.
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of all measurements"""
+    # Query all precip
+    results = session.query(Measurements.date, Measurements.prcp).all() #to get the station name column
+    print (results[0])
+   
+    precip = {date: prcp for date, prcp in results}
+
+    return jsonify(precip)
+
+    session.close() # good housekeeping to close session
+
+#################################################
+# * Query the dates and temperature observations of the most active station for the last year of data.
+# * Return a JSON list of temperature observations (TOBS) for the previous year.
+@app.route("/api/v1.0/tobs")
+def tobs():
+    """Return a list for most active station for last year"""
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # Query all
+    results = session.query(Measurements.tobs).\
+        filter(Measurements.date>='2016-08-23').\
+        filter(Measurements.station == 'USC00519281').all()
+
+    session.close() # good housekeeping to close session or add end only??
+    print(results)
+
+# Convert list of tuples into normal list
+    tobs = list(np.ravel(results))
+
+# * Return a JSON list of stations from the dataset.
+    return jsonify(tobs=tobs)
+
+#################################################
+# * Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
+# * When given the start only, calculate `TMIN`, `TAVG`, and `TMAX` for all dates greater than and equal to the start date.
+# * When given the start and the end date, calculate the `TMIN`, `TAVG`, and `TMAX` for dates between the start and end date inclusive.
+#Austin: So in the URL the user is going to have to type in a date in the %Y-%m-%d format like so: http://127.0.0.1:5000/api/v1.0/2017-03-04 and it'll grab the min, avg, max
+
+@app.route("/api/v1.0/<start>")
+@app.route("/api/v1.0/<start>/<end>")
+def stats(start=None, end=None):
+    session = Session(engine)
+
+    # Query min, avg, max temp for a start & end range
+    # use Select statement
+    sel = [func.min(Measurements.tobs), func.avg(Measurements.tobs), func.max(Measurements.tobs)]
+    results = session.query(*sel).filter(Measurements.date >= start).all()
+    
+    # if start > "2016-08-23", results
+    #     else
+
+    session.close() # good housekeeping to close session
+    print(results)
+
+# # Convert list of tuples into normal list
+    stats = list(np.ravel(results))
+
+# # * Return a JSON list of stations from the dataset.
+    return jsonify(stats=stats)
 
 if __name__ == "__main__":
     # Create your app.run statement here
     app.run(debug=True)
 
-### STILL TO DO
-# NEW APP.ROUTE?
-# * Convert the query results to a dictionary using `date` as the key and `prcp` as the value.
-# * Return the JSON representation of your dictionary.
-
-# @app.route("/api/v1.0/measurements")
-# def measurements():
-#     # Create our session (link) from Python to the DB
-#     session = Session(engine)
-
-#     """Return a list of all station names"""
-#     # Query all temps
-#     results = session.query(measurements.station).all() #to get all data?
-#     session.close() # good housekeeping to close session
-
-#     all_results = []
-#     for temp in results:
-#         temps = {}
-#         temps["date"] = item[1]
-#         temps["prcp"] = float(item[2])
-#         all_results.append(temps)
-
-#     return jsonify(all_results)
-
-
-
-# * Query the dates and temperature observations of the most active station for the last year of data.
-# * Return a JSON list of temperature observations (TOBS) for the previous year.
-# *`/api/v1.0/<start>` and `/api/v1.0/<start>/<end>`
-# * Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
-# * When given the start only, calculate `TMIN`, `TAVG`, and `TMAX` for all dates greater than and equal to the start date.
-# * When given the start and the end date, calculate the `TMIN`, `TAVG`, and `TMAX` for dates between the start and end date inclusive.
-# ## Hints
+## Hints
 # * You will need to join the station and measurement tables for some of the queries.
-# * Use Flask `jsonify` to convert your API data into a valid JSON response object.
